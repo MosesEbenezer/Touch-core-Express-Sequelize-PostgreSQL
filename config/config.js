d1a6d3ae6fe0114b4f20e151c38c73d988b82a52
Moses Ebenezer <moseschimdike@gmail.com>
2021-06-15 20:03:38 +0100

    postgress

---
+++ config/config.js
@@ -0,0 +1,47 @@
+require('dotenv').config(); // this is important!
+module.exports = {
+    
+"development": {
+    "username": "root" || process.env.DBUsername,
+    "password": "" || process.env.DBPassword,
+    "database": "touch-core" || process.env.DBName,
+    "host": "127.0.0.1" || process.env.DBHost,
+    "port": 5432 || process.env.DBPort,
+    "dialect": "postgres",
+    // Use a different storage. Default: none
+    "seederStorage": "json",
+    // Use a different file name. Default: sequelize-data.json
+    "seederStoragePath": "sequelizeData.json",
+    // Use a different table name. Default: SequelizeData
+    "seederStorageTableName": "sequelize_data"
+},
+"test": {
+    "username": "root" || process.env.DBUsername,
+    "password": "" || process.env.DBPassword,
+    "database": "touch-core" || process.env.DBName,
+    "host": "127.0.0.1" || process.env.DBHost,
+    "port": 5432 || process.env.DBPort,
+    "dialect": "postgres",
+    // Use a different storage. Default: none
+    "seederStorage": "json",
+    // Use a different file name. Default: sequelize-data.json
+    "seederStoragePath": "sequelizeData.json",
+    // Use a different table name. Default: SequelizeData
+    "seederStorageTableName": "sequelize_data"
+},
+"production": {
+    "username": "root" || process.env.DBUsername,
+    "password": "" || process.env.DBPassword,
+    "database": "touch-core" || process.env.DBName,
+    "host": "127.0.0.1" || process.env.DBHost,
+    "port": 5432 || process.env.DBPort,
+    "dialect": "postgres",
+    // Use a different storage. Default: none
+    "seederStorage": "json",
+    // Use a different file name. Default: sequelize-data.json
+    "seederStoragePath": "sequelizeData.json",
+    // Use a different table name. Default: SequelizeData
+    "seederStorageTableName": "sequelize_data"
+}
+};
+
