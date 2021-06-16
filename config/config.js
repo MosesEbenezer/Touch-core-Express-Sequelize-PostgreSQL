dbc0c5452740903be5c20c68eb755baf1b47f5ea
Moses Ebenezer <moseschimdike@gmail.com>
2021-06-16 11:56:39 +0100

    config js change

--- config/config.js
+++ config/config.js
@@ -2,9 +2,9 @@ require('dotenv').config(); // this is important!
 module.exports = {
     
 "development": {
-    "username": "root" || process.env.DBUsername,
-    "password": "" || process.env.DBPassword,
-    "database": "" || process.env.DBName,
+    "username": "postgres" || process.env.DBUsername,
+    "password": "postgres" || process.env.DBPassword,
+    "database": "touchcore" || process.env.DBName,
     "host": "127.0.0.1" || process.env.DBHost,
     "port": 5432 || process.env.DBPort,
     "dialect": "postgres",
@@ -16,9 +16,9 @@ module.exports = {
     "seederStorageTableName": "sequelize_data"
 },
 "test": {
-    "username": "root" || process.env.DBUsername,
-    "password": "" || process.env.DBPassword,
-    "database": "touch-core" || process.env.DBName,
+    "username": "postgres" || process.env.DBUsername,
+    "password": "postgres" || process.env.DBPassword,
+    "database": "touchcore" || process.env.DBName,
     "host": "127.0.0.1" || process.env.DBHost,
     "port": 5432 || process.env.DBPort,
     "dialect": "postgres",
@@ -30,9 +30,9 @@ module.exports = {
     "seederStorageTableName": "sequelize_data"
 },
 "production": {
-    "username": "root" || process.env.DBUsername,
-    "password": "" || process.env.DBPassword,
-    "database": "touch-core" || process.env.DBName,
+    "username": "postgres" || process.env.DBUsername,
+    "password": "postgres" || process.env.DBPassword,
+    "database": "touchcore" || process.env.DBName,
     "host": "127.0.0.1" || process.env.DBHost,
     "port": 5432 || process.env.DBPort,
     "dialect": "postgres",
